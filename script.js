41433f76fbe7bfe3f193600c0634f9f1d8c90ffe
Nyle-Blake <Nyleblake1@gmail.com>
2024-02-05 13:22:22 +0000

    Remove old operator function sand operate functions and add new functions.
    Add new calculate, addDigit, addOperator, decimalPoint, getResult functions,
    and clearInputScreen functions.
    
    Add global firstNum, secondNum, and round variables
    The round variable keeps says wether the current calculation was the
    first or not.
    
    This changed method works so that the first lot of numbers inputted into
    lowerInput is == firstNum and after the operator is added the lowerInput
    gets pushed up to the upperInput along with the operator and what gets
    put into lowerInput next is == secondNum. When equals is clicked the lowerInput
    is added to the upperInput with the previous text and the result of the
    calculate function is put into the lowerInput, then round is incremented.
    else if round !== 1 but operator == true
    The lowerInput which would be the result of the last calculation is
    pushed up to upperInput with the chosen operator and secondNum. Then the
    calculate will run.

--- script.js
+++ script.js
@@ -1,48 +1,93 @@
-const userInput = document.querySelector('.input')
-const calcResult = document.querySelector('.result')
+const upperInput = document.querySelector('.upper-input')
+const lowerInput = document.querySelector('.lower-input')
 const numBtn = document.querySelectorAll('.num-btn')
 const operatorBtn = document.querySelectorAll('.operator-btn')
 const acBtn = document.querySelector('#ac-btn')
 const decimalBtn = document.querySelector('#decimal-btn')
 const equalsBtn = document.querySelector('#equals-btn')
 
-const clearInputScreen = () => {
-  return userInput.textContent = ''
-};
+let operator;
+let firstNum, secondNum, round = 1;
 
-const addition = (a, b) => {
-  return parseFloat(a) + parseFloat(b);
-};
+const addDigit = (number) => {
+  lowerInput.innerText.concat("", number)
+}
 
-const subtraction = (a, b) => {
-  return parseFloat(a) - parseFloat(b);
-};
-const multiplication = (a, b) => {
-  return parseFloat(a) * parseFloat(b);
-};
-
-const division = (a, b) => {
-  return parseFloat(a) / parseFloat(b);
-};
-
-const operate = (a, b, operator) => {
-  if (operator = "+") {
-    return addition(a, b)
-  } else if (operator = "-") {
-    return subtraction(a, b)
-  } else if (operator = "*") {
-    return multiplication(a, b)
-  } else if (operator = "/") {
-    return division(a, b)
+const addOperator = (operatorValue) => {
+  if (lowerInput.textContent) {
+    round = 1;
+    operator = operatorValue;
+    firstNum = lowerInput.textContent;
+    upperInput.textContent = lowerInput.textContent.concat("", operator);
+    lowerInput.textContent = '';
   }
+}
+
+const calculate = (firstNum, secondNum, operator) => {
+  let result;
+  let a = firstNum;
+  let b = secondNum;
+
+  if (operator = '+') { result = a + b}
+  else if (operator = '-') { result = a - b}
+  else if (operator = '*') { result = a * b}
+  else if (operator = '/') { result = a / b}
+  else { } // no operator selected so do nothing
+
+  return result
+}
+
+const getResult = () => {
+  if (operator && round == 1) {
+    upperInput.textContent = upperInput.textContent.concat("", lowerInput.textContent)
+    secondNum = lowerInput.textContent
+    lowerInput.textContent = calculate(firstNum, secondNum, operator)
+    round++
+  } else if (operator) {
+    upperInput.textContent = `${lowerInput.textContent} ${operator} ${secondNum}`
+    firstNum = lowerInput.textContent
+    lowerInput.textContent = operator(firstNum, secondNum)
+  } else {
+    //do nothing because there is no operator
+  }
+}
+
+const deciamlPoint = () => {
+  if (!(lowerInput.textContent.includes('.') )) {
+    lowerInput.textContent = lowerInput.concat('.')
+  }
+};
+
+const clearInputScreen = () => {
+  upperInput.textContent = ''
+  lowerInput.textContent = ''
+  firstNum = undefined
+  secondNum = undefined
+  round = 1
 };
 
 numBtn.forEach((button) => {
   button.addEventListener('click', () => {
-    userInput.textContent = userInput.textContent + button.value  
+    addDigit(button.value)
+  })
+});
+
+operatorBtn.forEach((button) => {
+  button.addEventListener('click', () => {
+    addOperator(button.value)
   })
 });
 
 acBtn.addEventListener('click', () => {
-  return clearInputScreen()
+  clearInputScreen()
 });
+
+deciamlPoint.addEventListener('click', () => {
+  deciamlPoint()
+})
+
+// equalsBtn.addEventListener('click', () => {
+//   clearInputScreen()
+//   console.log(upperInput.textContent)
+//   console.log(currentOperator)
+// })
